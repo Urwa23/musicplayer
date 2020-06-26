169d2eb65de98d27ba75d1f2fa8c88615fba3dfe
Urwa Maqsood <56503227+Urwa23@users.noreply.github.com>
2020-06-27 00:50:40 +0500

    first commit

---
+++ music.js
@@ -0,0 +1,34 @@
+const boxes=document.querySelectorAll(".box");
+// console.log("wow")
+const gradients=["linear-gradient(to right, #ed213a, #93291e)",
+" linear-gradient(to right, #74ebd5, #acb6e5)",
+"linear-gradient(to right, #d9a7c7, #fffcdc)",
+"linear-gradient(to right, #ed213a, #93291e)",
+" linear-gradient(to right, #74ebd5, #acb6e5)",
+"linear-gradient(to right, #d9a7c7, #fffcdc)"];
+const clicked=function(e){
+    // console.log(this);
+    // console.log("this");
+    if(e.propertyName.includes('flex')){
+    this.classList.toggle('class-active');
+    const gradientIndex=this.getAttribute('data-index');
+    console.log(gradientIndex);
+    this.style.background=gradients[gradientIndex];
+
+    // console.log(audio);
+    // console.log(this);
+    // console.log(e);
+}
+    // this.style.backgroundColor="black";
+
+
+}
+const clickednot=function(){
+    this.classList.toggle('open');
+
+}
+boxes.forEach(box=>box.addEventListener('transitionend',clicked));
+boxes.forEach(box=>box.addEventListener('click',clickednot));
+const audio=document.querySelectorAll("audio");
+
+
